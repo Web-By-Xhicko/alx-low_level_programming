bf5f912c0079002ff9a7fd62034ef61b31c77e3c
Otagada Isaac <90391743+Xhicko@users.noreply.github.com>
2022-10-14 01:00:29 +0100

    fixing 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include <stdlib.h>
 #include "function_pointers.h"
-
+#include<stdlib.h>
 /**
  * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
+ * @name: person's name
+ * @f: pointer to a function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+if (f != NULL)
+f(name);
+
 }
